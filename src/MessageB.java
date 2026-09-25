7202b0dd348b9655e5a7fe3644efea132b2135da
PHANOU💕 <moffophanuellechloe@gmail.com>
2026-09-25 14:57:14 +0100

    ajout dune deuxieme ligne au message de B

--- src/MessageB.java
+++ src/MessageB.java
@@ -1,5 +1,5 @@
 public class MessageB {
  public static void main(String[] args) {
- System.out.println("Message du membre B");
+ System.out.println("ajout d'une deuxieme ligne du message de B");
  }
 }
