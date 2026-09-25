4e4bd6de6b901d25e27e58d7674189fe7681ab6d
Maélyse SOHOUNDE <sohoundemaelyse@gmail.com>
2026-09-25 15:30:14 +0200

    Ajout d'une deuxième ligne au message de A

--- src/MessageA.java
+++ src/MessageA.java
@@ -1,5 +1,7 @@
 public class MessageA {
  public static void main(String[] args) {
- System.out.println(" Maelyse SOHOUNDE : Bonjour a tous !");
+ System.out.println("Message du membre A");
+ System.out.println("Deuxième ligne de A");
  }
 }
+
